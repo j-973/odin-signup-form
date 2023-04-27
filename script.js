eebae4039eb4721acd1f1d5c7645f21932b89a8c
j-973 <jvoorheis973@gmail.com>
2023-04-27 15:52:09 -0400

    added resetForm function to clear messages and new css style rules notifying user of invalid password. added validatePwdTyping and validateOnSubmit to check password against regex pattern, prevent form submission, and apply/remove input box css styles when appropriate.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const PWD_REQS = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,16}$/;
+const FORM = document.getElementById("new_user"),
+    PWD_REQS = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,16}$/,
     PWD = document.getElementById("pwd"),
     CONFIRM_PWD = document.getElementById("confirm_pwd"),
     MSG_INVALID = document.getElementById("msg_invalid"),
     BTN_SUBMIT = document.getElementById("submit_new_user");
 
-    //if the value of the user-entered password is valid (sticks to the regex rules), then the invalid message stays blank, and return true. Otherwise, prevent the default behavior of submitting the form for the button, change the error text and return false;
-    checkPwd = (ev) => {
-        if (PWD_REQS.test(PWD.value)) {
+    //If the password and confirm password both meet the regex requirments, submit the form and keep the invalid message blank, otherwise prevent form submission with preventDefault(), add the .error css rules to password inputs, and show error message
+    validatePwd = (ev) => {
+        if (PWD_REQS.test(PWD.value) && PWD_REQS.test(CONFIRM_PWD.value)) {
             MSG_INVALID.textContent = ``;
             return true; 
         }
@@ -17,9 +18,35 @@ const PWD_REQS = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,
                 CONFIRM_PWD.classList.add(`error`);
             }
             MSG_INVALID.textContent = `*Passwords must be between 8-16 characters, and contain at least 
-            one lowercase letter, one uppercase letter, one digit, and one special character (!@#$%^&*)`
+            one lowercase letter, one uppercase letter, one digit, and one special character (!@#$%^&*)`;
             return false; 
         }
     }
 
-BTN_SUBMIT.addEventListener("click", checkPwd);
+    resetForm = (ev) => {
+        if (PWD.classList.contains(`error`) || CONFIRM_PWD.classList.contains(`error`)) {
+            PWD.classList.remove(`error`);
+            CONFIRM_PWD.classList.remove(`error`);
+        }
+        PWD.value = ``;
+        CONFIRM_PWD.value = ``;
+        MSG_INVALID.textContent = ``;
+    }
+
+    validatePwdTyping = (ev) => {
+            if (ev.target.matches('#pwd') || ev.target.matches('#confirm_pwd')) {
+                validatePwd(ev);
+        }
+    }
+
+    validateOnSubmit = (ev) => {
+        ev.preventDefault();
+        if (validatePwd(ev)) {
+            FORM.submit();
+        } else {
+            resetForm(ev);
+        }
+    }
+
+FORM.addEventListener("input", validatePwdTyping);
+FORM.addEventListener("submit", validateOnSubmit);
